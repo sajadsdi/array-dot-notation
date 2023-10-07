60369062240d81655b141886f85f25f83d5d2319
SajaD SaeeDi <thunder11like@gmail.com>
2023-10-08 01:00:51 +0330

    [Update] DotNotationTest , add new test for new features.

--- tests/DotNotationTest.php
+++ tests/DotNotationTest.php
@@ -1,4 +1,5 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 use Sajadsdi\ArrayDotNotation\DotNotation;
 
@@ -9,7 +10,7 @@ class DotNotationTest extends TestCase
         $data = [
             'user' => [
                 'profile' => [
-                    'id' => 625,
+                    'id'  => 625,
                     'pic' => '625.png',
                 ],
             ],
@@ -24,8 +25,8 @@ class DotNotationTest extends TestCase
     public function testGetMultipleValues()
     {
         $settings = [
-            'app' => [
-                'name' => 'My App',
+            'app'  => [
+                'name'    => 'My App',
                 'version' => '1.0',
             ],
             'user' => [
@@ -35,11 +36,11 @@ class DotNotationTest extends TestCase
 
         $dotNotation = new DotNotation($settings);
 
-        $result = $dotNotation->get('app.name', 'app.version', 'user');
+        $result   = $dotNotation->get(['app.name', 'app.version', 'user']);
         $expected = [
-            'name' => 'My App',
+            'name'    => 'My App',
             'version' => '1.0',
-            'user' => [
+            'user'    => [
                 'theme' => 'light',
             ],
         ];
@@ -52,7 +53,7 @@ class DotNotationTest extends TestCase
         $data = [
             'user' => [
                 'profile' => [
-                    'id' => 625,
+                    'id'  => 625,
                     'pic' => '625.png',
                 ],
             ],
@@ -72,7 +73,7 @@ class DotNotationTest extends TestCase
         $data = [
             'user' => [
                 'profile' => [
-                    'id' => 625,
+                    'id'  => 625,
                     'pic' => '625.png',
                 ],
             ],
@@ -90,7 +91,7 @@ class DotNotationTest extends TestCase
         $data = [
             'user' => [
                 'profile' => [
-                    'id' => 625,
+                    'id'  => 625,
                     'pic' => '625.png',
                 ],
             ],
@@ -100,7 +101,7 @@ class DotNotationTest extends TestCase
 
         $keys = ['profile_id' => 'user.profile.id', 'profile_photo' => 'user.profile.pic'];
 
-        $result = $dotNotation->get($keys);
+        $result   = $dotNotation->get($keys);
         $expected = ['profile_id' => 625, 'profile_photo' => '625.png'];
 
         $this->assertEquals($expected, $result);
@@ -122,7 +123,7 @@ class DotNotationTest extends TestCase
 
         $keys = ['users.2.name', 'users.3.name', 'users.4.name'];
 
-        $result = $dotNotation->get($keys);
+        $result   = $dotNotation->get($keys);
         $expected = ['name' => 'Emma', 'name_1' => 'Emily', 'name_2' => 'Sofia'];
 
         $this->assertEquals($expected, $result);
@@ -144,9 +145,90 @@ class DotNotationTest extends TestCase
 
         $keys = ['users.2', 'users.3', 'users.4'];
 
-        $result = $dotNotation->get($keys);
+        $result   = $dotNotation->get($keys);
         $expected = [['id' => 3, 'name' => 'Emma'], ['id' => 4, 'name' => 'Emily'], ['id' => 5, 'name' => 'Sofia']];
 
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testMultiCheckKeyExistence()
+    {
+        $data = [
+            'user' => [
+                'profile' => [
+                    'id'  => 625,
+                    'pic' => '625.png',
+                ],
+            ],
+        ];
+
+        $dotNotation = new DotNotation($data);
+
+        $keysExists = $dotNotation->has(['user.profile.id', 'user.profile.pic']);
+        $this->assertTrue($keysExists);
+
+        $keysNotExists = $dotNotation->has(['user.profile.id', 'user.profile.name']);
+        $this->assertFalse($keysNotExists);
+    }
+
+    public function testHasOneKeyExistence()
+    {
+        $data = [
+            'user' => [
+                'profile' => [
+                    'id'  => 625,
+                    'pic' => '625.png',
+                ],
+            ],
+        ];
+
+        $dotNotation = new DotNotation($data);
+
+        $keyExists = $dotNotation->hasOne(['user.profile.name', 'user.profile.pic']);
+        $this->assertTrue($keyExists);
+
+        $keyNotExists = $dotNotation->hasOne(['user.profile.name', 'user.profile.uuid']);
+        $this->assertFalse($keyNotExists);
+    }
+
+    public function testGetWithDefault()
+    {
+        $settings = [
+            'app'  => [
+                'name'    => 'My App',
+                'version' => '1.0',
+            ],
+            'user' => [
+                'theme' => 'light',
+            ],
+        ];
+
+        $dotNotation = new DotNotation($settings);
+
+        //get single key with default
+        $result = $dotNotation->get('user.name', 'Test User');
+        $this->assertEquals('Test User', $result);
+        //if key is set
+        $result = $dotNotation->get('user.theme', 'test');
+        $this->assertEquals('light', $result);
+
+        //get multiple keys with single default
+        $result   = $dotNotation->get(['app.version', 'user.theme', 'user.name'], 'test');
+        $expected = [
+            'version' => '1.0',
+            'theme'   => 'light',
+            'name'    => 'test'
+        ];
+        $this->assertEquals($expected, $result);
+
+        //get multiple keys with multiple default
+        $result   = $dotNotation->get(['app.env', 'user.profile.pic', 'user.name', 'user.theme'], ['local', '625.png', 'test user', 'night']);
+        $expected = [
+            'env'   => 'local',
+            'pic'   => '625.png',
+            'name'  => 'test user',
+            'theme' => 'light'
+        ];
         $this->assertEquals($expected, $result);
     }
 }
